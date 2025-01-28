85262c8e7d942a28995233138d0ccb4f9ee41c20
zaidoune1 <81720068+zaidoune1@users.noreply.github.com>
2025-01-28 17:02:06 +0100

    Add function getFieldFromCart to retrieve nested fields from the cart object using a path of keys

--- src/eligibility.service.js
+++ src/eligibility.service.js
@@ -7,6 +7,22 @@ class EligibilityService {
    * @param criteria
    * @return {boolean}
    */
+
+  /**
+   * @param {Object} cartSection - The section of the cart being examined.
+   * @param {Array} keys - The path representing the keys to the target field.
+   * @return {any} - The value of the field, or throws an error if not found.
+   */
+  getFieldFromCart(cartSection, keys) {
+    if (!cartSection || !cartSection.hasOwnProperty(keys[0])) {
+      if (Array.isArray(cartSection)) {
+        return cartSection.map((item) => item[keys[0]]);
+      }
+      throw new Error(`Key not found: ${keys[0]}`);
+    }
+    if (keys.length === 1) return cartSection[keys[0]];
+    return this.getFieldFromCart(cartSection[keys[0]], keys.slice(1));
+  }
   isEligible(cart, criteria) {
     // TODO: compute cart eligibility here.
     return false;
